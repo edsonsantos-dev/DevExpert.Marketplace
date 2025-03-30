4e211560933c20022ae244c5ddf7ec032763b40c
Edson Santos <dev@escode.net>
2025-03-30 14:25:10 -0300

    Switch to MVC and configure default routing
    Replaced Razor Pages with MVC by adding controllers with views and defining a default route. Updated exception handling to point to "/Home/Error" for consistency with MVC. Enhanced static asset mapping compatibility with both Razor Pages and MVC routes.

--- src/DevExpert.Marketplace.App/Program.cs
+++ src/DevExpert.Marketplace.App/Program.cs
@@ -12,27 +12,33 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<IdentityContext>();
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 app.MapRazorPages()
     .WithStaticAssets();
 
