b1a89ca427eeff595a3b7ec704aeb1f35c6dedee
Edson Santos <dev@escode.net>
2025-05-09 04:54:47 -0300

    Update UI text and labels for localization to Portuguese
    Translated UI text, labels, and error messages from English to Portuguese across Identity Management pages. This improves user experience for Portuguese-speaking users.

--- src/DevExpert.Marketplace.App/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ src/DevExpert.Marketplace.App/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -54,7 +54,7 @@ namespace DevExpert.Marketplace.App.Areas.Identity.Pages.Account.Manage
             /// </summary>
             [Required]
             [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
+            [Display(Name = "Senha atual")]
             public string OldPassword { get; set; }
 
             /// <summary>
@@ -62,9 +62,9 @@ namespace DevExpert.Marketplace.App.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "A {0} deve ter pelo menos {2} e no máximo {1} caracteres.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
+            [Display(Name = "Nova senha")]
             public string NewPassword { get; set; }
 
             /// <summary>
@@ -72,8 +72,8 @@ namespace DevExpert.Marketplace.App.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Display(Name = "Confirmar nova senha")]
+            [Compare("NewPassword", ErrorMessage = "A nova senha e a confirmação da senha não correspondem.")]
             public string ConfirmPassword { get; set; }
         }
 
